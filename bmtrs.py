8fadbf1aadd8fe7da1994ce74267d7a280b7e28c
Neha Deshpande <neha.n.deshpande@gmail.com>
2018-07-09 21:52:50 +0200

    Update bmtrs.py

--- bmtrs.py
+++ bmtrs.py
@@ -1,26 +1,23 @@
 from tkinter import *
-import pymysql as mdb
-import pandas as pd
-import time
-from tkinter import messagebox
-import datetime
-import re
-import os
 
-#Define a class to set up our GUI
-class BmtrsApp(object):
-    def __init__(self, login_screen):
-    	 #Set the window title
-        login_screen.wm_title("BMTRS")
-        
+# Define a class to set up our GUI
 
-#In our main function, create the GUI and pass it to our App class
+
+class App(object):
+    def __init__(self, window):
+        # Set the window title
+        window.wm_title("Database Interface")
+
+# In our main function, create the GUI and pass it to our App class
+
+
 def main():
-	login_screen= Tk()
-	start= App(login_screen)
-	login_screen.mainloop()
-	input('Press ENTER to exit')
+    window= Tk()
+    start= App(window)
+    window.mainloop()
 
-#Run the main function
+#  Run the main function
+
+
 if __name__ == "__main__":
     main()
