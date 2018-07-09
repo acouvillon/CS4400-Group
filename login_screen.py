71da379520e473199bb9698fdec3f13ac50ee531
Neha Deshpande <neha.n.deshpande@gmail.com>
2018-07-09 23:50:41 +0200

    Update login_screen.py

--- login_screen.py
+++ login_screen.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 
 # Define a class to set up our GUI
 
@@ -6,21 +7,42 @@ from tkinter import *
 class App(object):
     def __init__(self, login_screen):
         # Set the window title
-        login_screen.wm_title("BMTRS Application")
-        email_label = Label(login_screen, text ="Email: ")
-        pwd_label = Label(login_screen, text ="Password: ")
-        email_entry = Entry(login_screen)
-        pwd_entry = Entry(login_screen)
-        # sticky = E makes sure it sticks to right end of the cell in the grid
-        email_label.grid(row=0, sticky=E)
-        email_entry.grid(row=0, column=1)
-        pwd_label.grid(row=1, sticky=E)
-        pwd_entry.grid(row=1, column=1)
-        login_button = Button(login_screen, text='Login', fg='blue')
-        # event = left mouse click
-        # function = open home screen
-        login_button.bind("<Button-1>", login_screen)
-        login_button.grid(columnspan=2)
+
+        self.parent = login_screen
+
+        self.f1_style = ttk.Style()
+        self.f1_style.configure('My.TFrame')
+        self.f1 = ttk.Frame(self.parent, style='My.TFrame', padding=(3, 3, 12, 12))  # added padding
+        self.f1.grid(column=1, row=2, sticky=(N, S, E, W))  # added sticky
+        title = Label(login_screen, text="BMTRS", font=("Verdana", 20)).grid(row=0, columnspan=5)
+        self.email_label = ttk.Label(self.f1, text="Email:")
+        self.email = ttk.Entry(self.f1)
+        self.pwd_label = ttk.Label(self.f1, text="Password:")
+        self.pwd = ttk.Entry(self.f1)
+
+        login_screen.wm_title("")
+
+        login_button = Button(self.f1, text="Login", fg='blue', width=20)
+        registration_button = Button(self.f1, text="New User? Click here to register", font=("Verdana 10 underline"), fg='blue', borderwidth=0, relief='flat')
+
+
+        self.f1.grid(column=1, row=2, sticky=(N, S, E, W))  # added sticky
+        self.email_label.grid(column=5, row=2, columnspan=1, sticky=(N, E), pady=5, padx=5)  # added sticky, padx
+        self.email.grid(column=6, row=2, columnspan=1, sticky=(N, E, W), pady=5, padx=5)  # added sticky, pady, padx
+        self.pwd_label.grid(column=5, row=3, columnspan=1, sticky=(N, E), pady=5, padx=5)  # added sticky, padx
+        self.pwd.grid(column=6, row=3, columnspan=1, sticky=(N, E, W), pady=5, padx=5)  # added sticky, pady, padx
+        login_button.grid(column=6, row=5)
+        registration_button.grid(column=6, row=6)
+
+        # added resizing configs
+        self.parent.columnconfigure(0, weight=1)
+        self.parent.rowconfigure(0, weight=1)
+        self.f1.columnconfigure(0, weight=1)
+        self.f1.columnconfigure(1, weight=1)
+        self.f1.columnconfigure(2, weight=1)
+        self.f1.columnconfigure(3, weight=1)
+        self.f1.columnconfigure(4, weight=1)
+        self.f1.rowconfigure(1, weight=1)
 
 # In our main function, create the GUI and pass it to our App class d
 
