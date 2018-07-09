20fd24e052c0a49f251a28f7c4dfb8bd22f31cfb
Neha Deshpande <neha.n.deshpande@gmail.com>
2018-07-09 23:52:30 +0200

    Update login_screen.py

--- login_screen.py
+++ login_screen.py
@@ -23,7 +23,7 @@ class App(object):
         login_screen.wm_title("")
 
         login_button = Button(self.f1, text="Login", fg='blue', width=20)
-        registration_button = Button(self.f1, text="New User? Click here to register", font=("Verdana 10 underline"), fg='blue', borderwidth=0, relief='flat')
+        registration_button = Button(self.f1, text="New User? Click here to register", font=("Verdana 8 underline"), fg='blue', borderwidth=0, relief='flat')
 
 
         self.f1.grid(column=1, row=2, sticky=(N, S, E, W))  # added sticky
@@ -31,8 +31,8 @@ class App(object):
         self.email.grid(column=6, row=2, columnspan=1, sticky=(N, E, W), pady=5, padx=5)  # added sticky, pady, padx
         self.pwd_label.grid(column=5, row=3, columnspan=1, sticky=(N, E), pady=5, padx=5)  # added sticky, padx
         self.pwd.grid(column=6, row=3, columnspan=1, sticky=(N, E, W), pady=5, padx=5)  # added sticky, pady, padx
-        login_button.grid(column=6, row=5)
-        registration_button.grid(column=6, row=6)
+        login_button.grid(column=6, row=5, pady=5)
+        registration_button.grid(column=6, row=6, pady=5)
 
         # added resizing configs
         self.parent.columnconfigure(0, weight=1)
@@ -43,6 +43,11 @@ class App(object):
         self.f1.columnconfigure(3, weight=1)
         self.f1.columnconfigure(4, weight=1)
         self.f1.rowconfigure(1, weight=1)
+
+
+
+
+
 
 # In our main function, create the GUI and pass it to our App class d
 
