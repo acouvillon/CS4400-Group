12a6095d07be6a28a61c5531790a54e394a6a8f3
Neha Deshpande <neha.n.deshpande@gmail.com>
2018-07-10 17:56:47 +0200

    Working Login screen and New User Registration
    Still need to implement some functionality
    Check #todo that I've added for tasks to complete and please start working on them

--- bmtrsapp.py
+++ bmtrsapp.py
@@ -1,132 +1,147 @@
+import tkinter as tk
 from tkinter import *
-from tkinter import ttk
-from tkinter import font
+
+LARGE_FONT = ("Verdana", 26, "underline")
+SMALL_FONT = ("Verdana", 10, "italic")
+class BMTRSApp(tk.Tk):
+
+    #self is implied -- it's the first parameter
+    #args = arguments
+    #keyboard arguments
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        login_screen = tk.Frame(self)
+        #fill fills space allotted
+        #expand beyond space allotted
+        login_screen.pack(side="top", fill="both", expand=True)
+        #0 - minimum,
+        login_screen.grid_rowconfigure(0, weight=1)
+        login_screen.grid_columnconfigure(0, weight=1)
+
+        self.frames ={}
+        for F in {LoginPage, RegistrationPage}:
+            login_frame = F(login_screen, self)
+            self.frames[F] = login_frame
+            #sticky alignment + stretch - so it aligns everything to all sides of window
+            login_frame.grid(row=0, column=0, sticky="nsew")
+        self.show_frame(LoginPage)
+
+    def show_frame(self, container):
+        frame = self.frames[container]
+        frame.tkraise()
+
+#todo
+#Checks database, validates credentials and allows access to specific museum page
+def login(username, pwd):
+    print("login successful")
 
 
-class BmtrsApp(object):
-    def __init__ (self, home_screen):
-        self.parent = home_screen
-        create_login_page()
-        home_screen.withdraw()
+#PAGE 1 - LOGIN PAGE
+class LoginPage(tk.Frame):
 
-def create_login_page():
-    login_page = Tk()
-    f1 = ttk.Frame(login_page, style='My.TFrame', padding=(3, 3, 12, 12))  # added padding
-    f1.grid(column=1, row=2, sticky=(N, S, E, W))  # added sticky
-    title = Label(login_page, text="BMTRS", font=("Verdana", 20)).grid(row=0, columnspan=5)
-    email_label = ttk.Label(f1, text="Email:")
-    email = ttk.Entry(f1)
-    email_text = StringVar()
-    pwd_label = ttk.Label(f1, text="Password:")
-    pwd = ttk.Entry(f1)
-    pwd_text = StringVar()
-
-
-
-    login_button = Button(f1, text="Login", fg='blue', width=20)
-    create_new_user_button = Button(f1, text="New User? Click here to register",
-                                    font="Verdana 8 underline", fg='blue', borderwidth=0, command=create_registration_page)
-
-    f1.grid(column=1, row=2, sticky=(N, S, E, W))  # added sticky
-    email_label.grid(column=5, row=2, columnspan=1, sticky=(N, E), pady=5, padx=5)  # added sticky, padx
-    email.grid(column=6, row=2, columnspan=1, sticky=(N, E, W), pady=5, padx=5)  # added sticky, pady, padx
-    pwd_label.grid(column=5, row=3, columnspan=1, sticky=(N, E), pady=5, padx=5)  # added sticky, padx
-    pwd.grid(column=6, row=3, columnspan=1, sticky=(N, E, W), pady=5, padx=5)  # added sticky, pady, padx
-    login_button.grid(column=6, row=9, pady=5)
-    create_new_user_button.grid(column=6, row=10, pady=5)
-
-    # added resizing configs
-    login_page.columnconfigure(0, weight=1)
-    login_page.rowconfigure(0, weight=1)
-    f1.columnconfigure(0, weight=1)
-    f1.columnconfigure(1, weight=1)
-    f1.columnconfigure(2, weight=1)
-    f1.columnconfigure(3, weight=1)
-    f1.columnconfigure(4, weight=1)
-    f1.columnconfigure(5, weight=1)
-    f1.columnconfigure(6, weight=1)
-    f1.columnconfigure(7, weight=1)
-    f1.rowconfigure(1, weight=1)
+    def __init__(self, parent, controller):
+            tk.Frame.__init__(self, parent)
+            title = tk.Label(self, text="BMTRS", font=LARGE_FONT)
+            title.pack(pady=10, padx=10)
+            black_line=Frame(self, height=1, width=500, bg="black")
+            black_line.pack()
+            information_entry_frame = tk.Frame(self, borderwidth=5, relief='groove', pady=10)
+            information_entry_frame.pack(anchor='center', pady=20, padx=5)
+            email_label = tk.Label(information_entry_frame, text="Email:", font=SMALL_FONT)
+            email_label.grid(row=0, column=0, sticky='e', pady=5, padx=5)
+            email_entry = tk.Entry(information_entry_frame)
+            email_entry.grid(row=0, column=1, sticky='w', pady=5, padx=5)
+            email_text=StringVar()
+            pwd_label = tk.Label(information_entry_frame, text="Password:", font=SMALL_FONT)
+            pwd_label.grid(row=1, column=0, sticky='e', pady=5, padx=5)
+            # u022 is code for dot so that, the user's password is not visible
+            pwd_entry = tk.Entry(information_entry_frame, show='\u2022')
+            pwd_entry.grid(row=1, column=1, sticky='w', pady=5, padx=5)
+            login_button = tk.Button(self, text="Login", fg='blue',
+                                     command=lambda: login(username_text, pwd_text))
+            register_button = tk.Button(self, borderwidth=0, text="New User? Click here to register",
+                                        font="Verdana 10 underline", fg='blue',
+                                        command=lambda: controller.show_frame(RegistrationPage))
+            login_button.pack(anchor='n', expand=True)
+            black_line=Frame(self, height=1, width=500, bg="black")
+            black_line.pack(anchor='n')
+            register_button.pack(pady=0, anchor='n')
 
 
-    login_page.wm_title("")
+class RegistrationPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        title = tk.Label(self, text="New User Registration", font=LARGE_FONT)
+        title.pack(anchor='center')
+        black_line=Frame(self, height=1, width=500, bg="black")
+        black_line.pack(pady=20)
+
+        information_entry_frame = tk.Frame(self, borderwidth=5, relief='groove')
+        information_entry_frame.pack(anchor='center', pady=20, padx=5)
+
+        email_label = tk.Label(information_entry_frame, text="Email:", font=SMALL_FONT)
+        email_label.grid(row=0, column=0, sticky='e', pady=5, padx=5)
+        email_text=StringVar()
+        email_entry = tk.Entry(information_entry_frame, textvariable=email_text)
+        email_entry.grid(row=0, column=1, sticky='w', pady=5, padx=5)
 
 
-def create_registration_page():
-    registration_page = Tk()
-    f1_style = ttk.Style()
-    f1_style.configure('My.TFrame')
-    f1 = ttk.Frame(registration_page, style='My.TFrame', padding=(3, 3, 12, 12))  # added padding
-    f1.grid(column=1, row=2, sticky=(N, S, E, W))  # added sticky
-    title = Label(registration_page, text="New User Registration", font=("Verdana", 20)).grid(row=0, columnspan=5)
-    email_label = ttk.Label(f1, text="Email:")
-    email = ttk.Entry(f1)
-    email_text = StringVar()
-    pwd_label = ttk.Label(f1, text="Password:")
-    pwd = ttk.Entry(f1)
-    pwd_text = StringVar()
-    confirm_pwd_label = ttk.Label(f1, text="Confirm Password:")
-    confirm_pwd = ttk.Entry(f1)
-    confirm_pwd_text = StringVar()
-    credit_card_num_label = ttk.Label(f1, text="Credit Card Number:")
-    credit_card_num = ttk.Entry(f1)
-    ccd_text = StringVar()
-    exp_date_label = ttk.Label(f1, text="Credit Card Exp. Date:")
-    exp_date = ttk.Entry(f1)
-    exp_text = StringVar()
-    security_code_label = ttk.Label(f1, text="Credit Card Security Code:")
-    security_code = ttk.Entry(f1)
-    sec_text = StringVar()
+        pwd_label = tk.Label(information_entry_frame, text="Password:", font=SMALL_FONT)
+        pwd_label.grid(row=1, column=0, sticky='e', pady=5, padx=5)
+        pwd_text = StringVar()
+        pwd_entry = tk.Entry(information_entry_frame, show="\u2022", textvariable=pwd_text)
+        pwd_entry.grid(row=1, column=1, sticky='w', pady=5, padx=5)
+
+        confirm_label = tk.Label(information_entry_frame, text="Confirm Password:", font=SMALL_FONT)
+        confirm_label.grid(row=2, column=0, sticky='e', pady=5, padx=5)
+        confirm_text=StringVar()
+        confirm_entry = tk.Entry(information_entry_frame, show="\u2022", textvariable=confirm_text)
+        confirm_entry.grid(row=2, column=1, sticky='w', pady=5, padx=5)
+
+        #todo - check if confirm text matches password text
+        credit_card = tk.Label(information_entry_frame, text="Credit Card Number:", font=SMALL_FONT)
+        credit_card.grid(row=3, column=0, sticky='e', pady=5, padx=5)
+        credit_card_text = StringVar()
+        credit_card_entry = tk.Entry(information_entry_frame, textvariable=credit_card_text)
+        credit_card_entry.grid(row=3, column=1, sticky='w', pady=5, padx=5)
+        #todo - credit card entry formatting as .... .... .... 3333
+        credit_card_text.trace("w", lambda name, index, mode, credit_card_text=credit_card_text: entryFormattingForCreditCardNumber(credit_card_entry))
+
+        exp_date = tk.Label(information_entry_frame, text="Credit Card Exp. Date:", font=SMALL_FONT)
+        exp_date.grid(row=4, column=0, sticky='e', pady=5, padx=5)
+        exp_date_text= StringVar()
+        #todo - format entry as mm/yy
+        exp_date_entry = tk.Entry(information_entry_frame, textvariable=exp_date_text)
+        exp_date_entry.grid(row=4, column=1, sticky='w', pady=5, padx=5)
+
+        security_code = tk.Label(information_entry_frame, text="Credit Card Security Code:", font=SMALL_FONT)
+        security_code.grid(row=4, column=0, sticky='e', pady=5, padx=5)
+        sec_code_text = StringVar()
+        #todo - format entry as 3 digit entry only
+        security_code_entry = tk.Entry(information_entry_frame, textvariable=sec_code_text)
+        security_code_entry.grid(row=4, column=1, sticky='w', pady=5, padx=5)
+
+        black_line=Frame(self, height=1, width=500, bg="black")
+        black_line.pack(pady=20)
+        create_account_button = tk.Button(self, text="Create Account", fg='blue',
+                                 command=lambda: create_account(email_text, pwd_text, credit_card_text, exp_date_text, sec_code_text))
+        back_button = tk.Button(self, text="Back", fg='blue', command=lambda: controller.show_frame(LoginPage))
+        create_account_button.pack(pady=5, anchor='n')
+        back_button.pack(pady=5, anchor='n')
+
+#todo - implement this function
+def create_account(email, pwd, credit_card_num, exp_date, security_code):
+    print('account created')
+
+def entryFormattingForCreditCardNumber(entry):
+    text = entry.get()
+    if len(text) % 4 == 0:
+        entry.insert(SEPARATOR, ' ')
+        entry.icursor(len(text)+1)
 
 
 
-    create_account_button = Button(f1, text="Create Account", fg='blue', width=20)
-    back_button = Button(f1, text="Back", fg='blue', width=20, command=lambda:[create_login_page,registration_page.withdraw])
-
-
-    f1.grid(column=1, row=2, sticky=(N, S, E, W))  # added sticky
-    email_label.grid(column=5, row=2, columnspan=1, sticky=(N, E), pady=5, padx=5)  # added sticky, padx
-    email.grid(column=6, row=2, columnspan=1, sticky=(N, E, W), pady=5, padx=5)  # added sticky, pady, padx
-    pwd_label.grid(column=5, row=3, columnspan=1, sticky=(N, E), pady=5, padx=5)  # added sticky, padx
-    pwd.grid(column=6, row=3, columnspan=1, sticky=(N, E, W), pady=5, padx=5)  # added sticky, pady, padx
-    confirm_pwd_label.grid(column=5, row=4, columnspan=1, sticky=(N, E), pady=5, padx=5)  # added sticky, padx
-    confirm_pwd.grid(column=6, row=4, columnspan=1, sticky=(N, E, W), pady=5, padx=5)  # added sticky, pady, padx
-    credit_card_num_label.grid(column=5, row=5, columnspan=1, sticky=(N, E), pady=5, padx=5)  # added sticky, padx
-    credit_card_num.grid(column=6, row=5, columnspan=1, sticky=(N, E, W), pady=5, padx=5)  # added sticky, pady, padx
-    exp_date_label.grid(column=5, row=6, columnspan=1, sticky=(N, E), pady=5, padx=5)  # added sticky, padx
-    exp_date.grid(column=6, row=6, columnspan=1, sticky=(N, E, W), pady=5, padx=5)  # added sticky, pady, padx
-    security_code_label.grid(column=5, row=7, columnspan=1, sticky=(N, E), pady=5, padx=5)  # added sticky, padx
-    security_code.grid(column=6, row=7, columnspan=1, sticky=(N, E, W), pady=5, padx=5)  # added sticky, pady, padx
-    create_account_button.grid(column=6, row=9, pady=5)
-    back_button.grid(column=6, row=10, pady=5)
-
-    # added resizing configs
-    registration_page.columnconfigure(0, weight=1)
-    registration_page.rowconfigure(0, weight=1)
-    f1.columnconfigure(0, weight=1)
-    f1.columnconfigure(1, weight=1)
-    f1.columnconfigure(2, weight=1)
-    f1.columnconfigure(3, weight=1)
-    f1.columnconfigure(4, weight=1)
-    f1.columnconfigure(5, weight=1)
-    f1.columnconfigure(6, weight=1)
-    f1.columnconfigure(7, weight=1)
-    f1.rowconfigure(1, weight=1)
-    registration_page.rowconfigure(2, weight=1)
-    registration_page.rowconfigure(3, weight=1)
-    registration_page.rowconfigure(4, weight=1)
-    registration_page.rowconfigure(5, weight=1)
-    registration_page.rowconfigure(6, weight=1)
-    registration_page.rowconfigure(7, weight=1)
-    registration_page.rowconfigure(8, weight=1)
-
-    registration_page.wm_title("")
-
-def main():
-    home_screen= Tk()
-    start= BmtrsApp(home_screen)
-    home_screen.mainloop()
-#  Run the main function
-
-if __name__ == "__main__":
-    main()
+app = BMTRSApp()
+#tkinter functionality keeps app running
+app.mainloop()
