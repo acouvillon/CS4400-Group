41681d482dc9bf90f582f73ff3bdb09bc3b043eb
Neha Deshpande <neha.n.deshpande@gmail.com>
2018-07-10 15:52:00 +0200

    Update and rename login_screen.py to bmtrsapp.py

--- bmtrsapp.py
+++ bmtrsapp.py
@@ -1,57 +1,59 @@
 from tkinter import *
 from tkinter import ttk
-
-# Define a class to set up our GUI
-class App(object):
-    def __init__(self, login_screen):
-        # Set the window title
-
-        self.parent = login_screen
-
-        self.f1_style = ttk.Style()
-        self.f1_style.configure('My.TFrame')
-        self.f1 = ttk.Frame(self.parent, style='My.TFrame', padding=(3, 3, 12, 12))  # added padding
-        self.f1.grid(column=1, row=2, sticky=(N, S, E, W))  # added sticky
-        title = Label(login_screen, text="BMTRS", font=("Verdana", 20)).grid(row=0, columnspan=5)
-        self.email_label = ttk.Label(self.f1, text="Email:")
-        self.email = ttk.Entry(self.f1)
-        self.email_text = StringVar()
-        self.pwd_label = ttk.Label(self.f1, text="Password:")
-        self.pwd = ttk.Entry(self.f1)
-        self.pwd_text = StringVar()
-
-        login_screen.wm_title("")
-
-        login_button = Button(self.f1, text="Login", fg='blue', width=20)
-        registration_button = Button(self.f1, text="New User? Click here to register",
-                                     font=("Verdana 8 underline"), fg='blue', borderwidth=0,
-                                     relief='flat', command=open_registration_page)
+from tkinter import font
 
 
-        self.f1.grid(column=1, row=2, sticky=(N, S, E, W))  # added sticky
-        self.email_label.grid(column=5, row=2, columnspan=1, sticky=(N, E), pady=5, padx=5)  # added sticky, padx
-        self.email.grid(column=6, row=2, columnspan=1, sticky=(N, E, W), pady=5, padx=5)  # added sticky, pady, padx
-        self.pwd_label.grid(column=5, row=3, columnspan=1, sticky=(N, E), pady=5, padx=5)  # added sticky, padx
-        self.pwd.grid(column=6, row=3, columnspan=1, sticky=(N, E, W), pady=5, padx=5)  # added sticky, pady, padx
-        login_button.grid(column=6, row=5, pady=5)
-        registration_button.grid(column=6, row=6, pady=5)
+class BmtrsApp(object):
+    def __init__ (self, home_screen):
+        self.parent = home_screen
+        create_login_page()
+        home_screen.withdraw()
 
-        # added resizing configs
-        self.parent.columnconfigure(0, weight=1)
-        self.parent.rowconfigure(0, weight=1)
-        self.f1.columnconfigure(0, weight=1)
-        self.f1.columnconfigure(1, weight=1)
-        self.f1.columnconfigure(2, weight=1)
-        self.f1.columnconfigure(3, weight=1)
-        self.f1.columnconfigure(4, weight=1)
-        self.f1.rowconfigure(1, weight=1)
+def create_login_page():
+    login_page = Tk()
+    f1 = ttk.Frame(login_page, style='My.TFrame', padding=(3, 3, 12, 12))  # added padding
+    f1.grid(column=1, row=2, sticky=(N, S, E, W))  # added sticky
+    title = Label(login_page, text="BMTRS", font=("Verdana", 20)).grid(row=0, columnspan=5)
+    email_label = ttk.Label(f1, text="Email:")
+    email = ttk.Entry(f1)
+    email_text = StringVar()
+    pwd_label = ttk.Label(f1, text="Password:")
+    pwd = ttk.Entry(f1)
+    pwd_text = StringVar()
 
 
-def open_registration_page():
-    create_registration_page()
+
+    login_button = Button(f1, text="Login", fg='blue', width=20)
+    create_new_user_button = Button(f1, text="New User? Click here to register",
+                                    font="Verdana 8 underline", fg='blue', borderwidth=0, command=create_registration_page)
+
+    f1.grid(column=1, row=2, sticky=(N, S, E, W))  # added sticky
+    email_label.grid(column=5, row=2, columnspan=1, sticky=(N, E), pady=5, padx=5)  # added sticky, padx
+    email.grid(column=6, row=2, columnspan=1, sticky=(N, E, W), pady=5, padx=5)  # added sticky, pady, padx
+    pwd_label.grid(column=5, row=3, columnspan=1, sticky=(N, E), pady=5, padx=5)  # added sticky, padx
+    pwd.grid(column=6, row=3, columnspan=1, sticky=(N, E, W), pady=5, padx=5)  # added sticky, pady, padx
+    login_button.grid(column=6, row=9, pady=5)
+    create_new_user_button.grid(column=6, row=10, pady=5)
+
+    # added resizing configs
+    login_page.columnconfigure(0, weight=1)
+    login_page.rowconfigure(0, weight=1)
+    f1.columnconfigure(0, weight=1)
+    f1.columnconfigure(1, weight=1)
+    f1.columnconfigure(2, weight=1)
+    f1.columnconfigure(3, weight=1)
+    f1.columnconfigure(4, weight=1)
+    f1.columnconfigure(5, weight=1)
+    f1.columnconfigure(6, weight=1)
+    f1.columnconfigure(7, weight=1)
+    f1.rowconfigure(1, weight=1)
+
+
+    login_page.wm_title("")
+
 
 def create_registration_page():
-    registration_page = Toplevel()
+    registration_page = Tk()
     f1_style = ttk.Style()
     f1_style.configure('My.TFrame')
     f1 = ttk.Frame(registration_page, style='My.TFrame', padding=(3, 3, 12, 12))  # added padding
@@ -79,7 +81,7 @@ def create_registration_page():
 
 
     create_account_button = Button(f1, text="Create Account", fg='blue', width=20)
-    back_button = Button(f1, text="Back", fg='blue', width=20, command=App(Tk()))
+    back_button = Button(f1, text="Back", fg='blue', width=20, command=lambda:[create_login_page,registration_page.withdraw])
 
 
     f1.grid(column=1, row=2, sticky=(N, S, E, W))  # added sticky
@@ -120,15 +122,11 @@ def create_registration_page():
 
     registration_page.wm_title("")
 
+def main():
+    home_screen= Tk()
+    start= BmtrsApp(home_screen)
+    home_screen.mainloop()
+#  Run the main function
 
-# In our main function, create the GUI and pass it to our App class d
-
-#def main():
-window= Tk()
-start= App(window)
-window.mainloop()
-    #  Run the main function
-
-#if __name__ == "__main__":
-    #main()
-
+if __name__ == "__main__":
+    main()
