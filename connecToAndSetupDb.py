d2b117bfa89f21d369e674c70fe2ab245de4cec2
Neha Deshpande <neha.n.deshpande@gmail.com>
2018-07-17 12:40:49 +0200

    Added all Alex's data
    Note: Check commented out sections and make sure the data is accurate

--- connecToAndSetupDb.py
+++ connecToAndSetupDb.py
@@ -1,10 +1,12 @@
 from __future__ import print_function
+import time
+import datetime
 import mysql.connector
 from mysql.connector import errorcode
 
 config = {
   'user': 'root',
-  'password': 'De5hNeh@',
+  'password': 'INSERT YOUR PASSWORD HERE',
   'database': 'bmtrsdb',
   'raise_on_warnings': True,
 }
@@ -113,17 +115,17 @@ TABLES['exhibit'] = (
 
 cursor = cnx.cursor()
 
-for name, ddl in TABLES.items():
-    try:
-        print("Creating table {}: ".format(name), end='')
-       cursor.execute(ddl)
-    except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
-            print("already exists.")
-       else:
-            print(err.msg)
-    else:
-        print("OK")
+#for name, ddl in TABLES.items():
+#    try:
+#        print("Creating table {}: ".format(name), end='')
+#       cursor.execute(ddl)
+#    except mysql.connector.Error as err:
+#        if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+#            print("already exists.")
+#       else:
+#            print(err.msg)
+#    else:
+#        print("OK")
 
 add_admin = ("INSERT INTO admin_user "
                "VALUES (%s, %s)")
@@ -131,40 +133,85 @@ add_visitor = ("INSERT INTO visitor "
                    "VALUES (%s, %s, %s, %s, %s, %s)")
 add_museum = ("INSERT INTO museum "
                   "VALUES(%s, %s)")
+add_curator_request = ("INSERT INTO curator_request "
+                           "VALUES (%s, %s)")
+add_review = ("INSERT INTO review "
+                  "VALUES(%s, %s, %s, %s)")
+add_ticket = ("INSERT INTO ticket "
+                  "VALUES (%s, %s, %s, %s)")
+add_exhibit = ("INSERT INTO exhibit "
+                   "VALUES (%s, %s, %s, %s)")
 #---------------------------ADMIN DATA----------------------------------
-data_admin = ('very_cool_guy@hotmail.com', 'imcool')
+#data_admin = ('alex@gatech.edu', 'iamadmin')
 # Insert new admin
-cursor.execute(add_admin, data_admin)
+#cursor.execute(add_admin, data_admin)
 
 #--------------------------VISITOR DATA----------------------------------
 # email, password, credit card num, expiry month, expiry year, security num
 data_visitor = []
-data_visitor.append(('themuseumguy@gmail.com', 'themuseumguy', '1111222233334444', 12, '1999', 666)) 
-for v in range(len(data_visitor)):
-    cursor.execute(add_visitor, data_visitor[v])
-    print("visitor added...")
-    v += 1
+#data_visitor.append(('themuseumguy@gmail.com', 'themuseumguy', '1111222233334444', 12, '1999', 666)) 
+#data_visitor.append(('daniel@gatech.edu', 'bilingual', '1234567812345670', 1, '2020', 123))
+#data_visitor.append(('helen@gatech.edu', 'architecture4ever', '8765432187654320', 2, '2021', 456))
+#data_visitor.append(('zoe@gatech.edu', 'yogasister', '2468135924681350', 3, '2022', 789))
+#for v in range(len(data_visitor)):
+#    cursor.execute(add_visitor, data_visitor[v])
+#    print("visitor added...")
+#    v += 1
 
 #--------------------------MUSEUM DATA-----------------------------------
 # museum name, curator email (optional)
 data_museum = []
-
+#data_museum.append(('MACBA', 'zoe@gatech.edu'))
+#data_museum.append(('Picasso Museum', None))
+#data_museum.append(('CCCB', 'helen@gatech.edu'))
+#for v in range(len(data_museum)):
+#    cursor.execute(add_museum, data_museum[v])
+#    print("museum added...")
+#    v += 1
+    
 #-------------------------CURATOR REQUEST DATA---------------------------
 # email, museum name
 data_curator_request = []
-
+#data_curator_request.append(('zoe@gatech.edu', 'Picasso Museum'))
+#for v in range(len(data_curator_request)):
+#    cursor.execute(add_curator_request, data_curator_request[v])
+#   print("curator_request added...")
+#    v += 1
 
 #-------------------------REVIEW DATA------------------------------------
 # email, museum name, comment (optional), rating
 data_review = []
+#data_review.append(('zoe@gatech.edu', 'MACBA', 'Didn\'t get it', 1))
+#data_review.append(('helen@gatech.edu', 'Picasso Museum', 'So many shapes!', 5))
+#data_review.append(('helen@gatech.edu', 'CCCB', 'Scary, but cool', 3))
+#for v in range(len(data_review)):
+#    cursor.execute(add_review, data_review[v])
+#    print("review added...")
+#    v += 1
 
 #-------------------------TICKET DATA------------------------------------
 # email, museum name, price, purchase_timestamp
 data_ticket = []
-
+#data_ticket.append(('zoe@gatech.edu', 'MACBA', 5, '2018-05-20'))
+#data_ticket.append(('helen@gatech.edu', 'Picasso Museum', 20, '2018-06-11'))
+#data_ticket.append(('helen@gatech.edu', 'CCCB', 20, '2018-06-29'))
+#for v in range(len(data_ticket)):
+#    cursor.execute(add_ticket, data_ticket[v])
+#    print("ticket added...")
+#    v += 1
 #-------------------------EXHIBIT DATA-----------------------------------
 # museum name, exhibit name, year, url
 data_exhibit = []
+#data_exhibit.append(('MACBA', 'Bird', '2018', 'www.macba.es/bird/'))
+#data_exhibit.append(('MACBA', 'Plane', '2018', 'www.macba.es/plane/'))
+#data_exhibit.append(('MACBA', 'Train', '2018', 'www.macba.es/train/'))
+#data_exhibit.append(('Picasso Museum', 'Geometric Shapes', '1900', 'www.picassomuseo.com/geo/'))
+#data_exhibit.append(('CCCB', 'Black Light 1', '1985', 'www.cccb.com/bl1'))
+#data_exhibit.append(('CCCB', 'Black Light 2', '1986', 'www.cccb.com/bl2'))
+#for v in range(len(data_exhibit)):
+#   cursor.execute(add_exhibit, data_exhibit[v])
+#    print("exhibit added...")
+#    v += 1
 
 # Make sure data is committed to the database
 cnx.commit()
